0009ef977b8a3ad24b393e29dbd075a524754538
Michael Fladischer <michael@fladi.at>
2019-05-28 13:03:00 +0200

    Initial split from outpost mono-repo.

---
+++ src/outpost/haystack.py
@@ -0,0 +1,13 @@
+from xapian_backend import XapianEngine, XapianSearchBackend
+
+
+class SearchBackend(XapianSearchBackend):
+    def update(self, index, iterable, commit=True):
+        return super().update(index, iterable)
+
+    def remove(self, obj, commit=True):
+        return super().remove(obj)
+
+
+class Engine(XapianEngine):
+    backend = SearchBackend
